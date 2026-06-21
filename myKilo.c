45316e03085d3389970e0fd466863f109d8ce7b7
EmanuelleBRL <lima.emanuelle11037@gmail.com>
2026-06-21 15:08:15 -0300

    feat(16): Turn off input & output processing; Turn off Extra Flags; Timeout for read

--- myKilo.c
+++ myKilo.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -7,7 +9,7 @@ struct termios orig_termios;
 
 void disableRawMode(){
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
-} /* TCSAFLUSH espera input pendente e descarta oq n foi lido; re-limpa terminal quando volta pro estado original*/
+} /* TCSAFLUSH espera input pendente e descarta oq n foi lido; re-limpa terminal quando volta pro estado original */
 
 
 
@@ -15,27 +17,44 @@ void disableRawMode(){
 
 void enableRawMode() {
 
-  tcgetattr(STDIN_FILENO, &orig_termios); /* read currient 
-                                          attriutes into a struct */
-  atexit(disableRawMode); /*chama função pra quando retornar do main
-  ou qnd exit(); ta dentro de enableRaw pq é quem gerencia o estado*/
+  tcgetattr(STDIN_FILENO, &orig_termios); /* Read currient attriutes into a struct */
+
+  atexit(disableRawMode); /* Chama função pra quando retornar do main ou qnd exit(); 
+                            Ta dentro de enableRaw pq é quem gerencia o estado */
 
 
-  struct termios raw = orig_termios; //copia estado atual do terminal para raw
+  struct termios raw = orig_termios; // Copia estado atual do terminal para raw
 
-  raw.c_lflag &= ~(ECHO | ICANON);   /* modifica strutc ~termios (pré-
-                               definida) pra desligar ECHO
-                               off canonmode ~ readingg byte-by-byte instead of line-by-line~*/
+  raw.c_iflag &= ~( BRKINT | ICRNL | INPCK | IXON );
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN );   /* Modifica strutc ~termios (pré-definida) pra desligar ECHO
+                                       Off canonmode ~ readingg byte-by-byte instead of line-by-line~ */
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
   
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); /* aplica mudança do echo
-                                               no terminal atual; 
-                                               limpa oq tiver no buffer~antes de read()*/
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); /* Aplica mudança do echo no terminal atual; 
+                                               Limpa oq tiver no buffer~antes de read() */
 
 }
 
 int main () { 
   enableRawMode();
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  
+  while (1){
+    char c = '\0';
+    read(STDIN_FILENO, &c, 1); 
+    if (iscntrl(c)){        /* Se for ctrl + algo, exibe apenas ASCII e não o comando 
+                               (qnd o terminal lẽ o char do comando, executa ele) */
+      printf("%d\r\n", c);
+    } else {
+      printf("%d ('%c')\r\n", c, c);
+    }
+    if (c == 'q') break;
+    }
+
+
+
+  
   return 0;}
 
