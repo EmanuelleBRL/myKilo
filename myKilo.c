626c38db0974b7e282205b5c7a082b1df4add1ca
EmanuelleBRL <lima.emanuelle11037@gmail.com>
2026-06-21 11:57:23 -0300

    feat(07): Disable Raw mode at exit; Turn Off canon mode

--- myKilo.c
+++ myKilo.c
@@ -1,14 +1,35 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+
+struct termios orig_termios;
+
+void disableRawMode(){
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+} /* TCSAFLUSH espera input pendente e descarta oq n foi lido; re-limpa terminal quando volta pro estado original*/
+
+
+
+
+
 void enableRawMode() {
-  struct termios raw;
 
-  tcgetattr(STDIN_FILENO, &raw);
+  tcgetattr(STDIN_FILENO, &orig_termios); /* read currient 
+                                          attriutes into a struct */
+  atexit(disableRawMode); /*chama função pra quando retornar do main
+  ou qnd exit(); ta dentro de enableRaw pq é quem gerencia o estado*/
 
-  raw.c_lflag &= ~(ECHO);
+
+  struct termios raw = orig_termios; //copia estado atual do terminal para raw
+
+  raw.c_lflag &= ~(ECHO | ICANON);   /* modifica strutc ~termios (pré-
+                               definida) pra desligar ECHO
+                               off canonmode ~ readingg byte-by-byte instead of line-by-line~*/
   
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); /* aplica mudança do echo
+                                               no terminal atual; 
+                                               limpa oq tiver no buffer~antes de read()*/
 
 }
 
