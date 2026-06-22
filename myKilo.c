c5750801f9e1088ed431b0572a734d2aa536fc45
EmanuelleBRL <lima.emanuelle11037@gmail.com>
2026-06-22 20:50:09 -0300

    feat(24): *Clear te screen -on exit-; Reposition the cursor

--- myKilo.c
+++ myKilo.c
@@ -22,6 +22,9 @@ struct termios orig_termios;
 /*** terminal ***/
 
 void die(const char *s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3); 
+
   perror(s);
   exit(1);
 }
@@ -68,6 +71,15 @@ char editorReadKey() {
   return c;
 }
 
+/*** output ***/
+
+void editorRefreshScreen() {          /** Escape Sequences do VT100**/
+  write(STDOUT_FILENO, "\x1b[2J", 4); /* Age como ctrl l (não /clear!) */
+  write(STDOUT_FILENO, "\x1b[H", 3); /* Retorna positção do cursor à coordenada 1,1 do tamanho do terminal */
+
+}
+
+
 /*** input ***/
 
 void editorProcessKeypress() {
@@ -75,6 +87,8 @@ void editorProcessKeypress() {
 
   switch (c) {
     case CTRL_KEY('q'):
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3); 
       exit(0);
       break;
   }
@@ -86,6 +100,7 @@ int main () {
   enableRawMode();
   
   while (1){
+   editorRefreshScreen();
    editorProcessKeypress(); 
   }
 
